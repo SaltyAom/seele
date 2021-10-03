b2d80e86f26724e1860d860cdc6e28b62d26c8a0
SaltyAom <aomkirby123@gmail.com>
2021-10-03 19:36:49 +0700

    :tada: feat: actix-4

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,3 +1,5 @@
+pub mod decoration;
 pub mod graphql;
 
+pub use decoration::use_decoration;
 pub use graphql::use_graphql;
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ mod models;
 mod controllers;
 mod services;
 
-use actix_web::{ HttpServer, App };
+use actix_web::{ HttpServer, App, web::Data };
+use actix_cors::Cors;
+use actix_files::NamedFile;
 
-use controllers::use_graphql;
+use controllers::{ use_decoration, use_graphql };
 use models::graphql::create_schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let schema = create_schema();
-
     HttpServer::new(move ||
         App::new()
-            .data(schema.clone())
+            .wrap(Cors::permissive().allow_any_origin())
+            .app_data(Data::new(schema.clone()))
             .configure(use_graphql)
+            .configure(use_decoration)
+            .default_service(NamedFile::open("public/index.html").unwrap())
     )
     .bind("0.0.0.0:8080")?
     .run()
